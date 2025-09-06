e4d35fdbf60d4133e13d9ecf3957416ffd7f3c89
Viet Le <vietlq85@gmail.com>
2025-09-06 11:56:02 +0100

    updated Gitsigns

--- neovim/init.lua
+++ neovim/init.lua
@@ -340,7 +340,48 @@ local plugins = {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true, desc = "Next git hunk"})
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true, desc = "Previous git hunk"})
+
+          -- Actions
+          map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage hunk" })
+          map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset hunk" })
+          map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Stage hunk" })
+          map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Reset hunk" })
+          map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage buffer" })
+          map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+          map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset buffer" })
+          map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview hunk" })
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = "Blame line" })
+          map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = "Toggle line blame" })
+          map('n', '<leader>hd', gs.diffthis, { desc = "Diff this" })
+          map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "Diff this ~" })
+          map('n', '<leader>td', gs.toggle_deleted, { desc = "Toggle deleted" })
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = "Select hunk" })
+        end
+      })
     end
   },
 
@@ -502,8 +543,8 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 -- Window splits
 vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Vertical split" })
 vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Horizontal split" })
-vim.keymap.set("n", "<leader>snv", ":vnew<CR>", { desc = "Vertical split with new buffer" })
-vim.keymap.set("n", "<leader>snh", ":new<CR>", { desc = "Horizontal split with new buffer" })
+vim.keymap.set("n", "<leader>snv", ":vnew<CR>", { desc = "New vertical split" })
+vim.keymap.set("n", "<leader>snh", ":new<CR>", { desc = "New horizontal split" })
 
 -- Edit commands
 vim.keymap.set("n", "<leader>ects", ":%s/\\s\\+$//e<CR>", { desc = "Clear trailing spaces" })
